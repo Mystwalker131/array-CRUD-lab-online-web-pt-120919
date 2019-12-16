62a7fb5131382c0bfe7988e7b0e4248567234a13
Benito Galvez <mystwalker131@gmail.com>
2019-12-16 14:57:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,8 +16,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  ["Alex", "Benny","Tyson","Dom"] .unshift("wow")
-end
+  
 end
 
 def remove_element_from_start_of_array(array)
