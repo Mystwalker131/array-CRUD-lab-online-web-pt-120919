ad0dedc83274936add52db2a7f51c2e2f8739c43
Benito Galvez <mystwalker131@gmail.com>
2019-12-16 14:53:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    ["Alex", "Benny","Tyson","Dom"] << "arrays!"
+  ["Alex", "Benny","Tyson","Dom"] << "arrays!"
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["Alex", "Benny","Tyson","Dom"]
 end
 
 def remove_element_from_end_of_array(array)
