66d493515ad96db3683c33ffd3b23105dce0498a
Benito Galvez <mystwalker131@gmail.com>
2019-12-16 15:11:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow","Alex", "Benny","Tyson","Dom", "arrays!"]
-  array = [1]
+  array = [am]
 end
 
 def retrieve_first_element_from_array(array)
